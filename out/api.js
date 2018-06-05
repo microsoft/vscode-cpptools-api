4cb388e2923a29b03cf01131a2635384021803d1
Bob Brown <bobbrow@microsoft.com>
2018-06-04 21:12:33 -0700

    generate api.d.ts

---
+++ out/api.js
@@ -0,0 +1,6 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All Rights Reserved.
+ * See 'LICENSE' in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
