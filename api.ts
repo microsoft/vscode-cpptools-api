135f413d2d9ea5d721a317a7511c5679e4dabd5e
Bob Brown (DEVDIV) <bobbrow@microsoft.com>
2019-07-24 15:26:38 -0700

    Add APIs for per-folder browse configs (#7)

--- api.ts
+++ api.ts
@@ -101,16 +101,33 @@ export interface CustomConfigurationProvider extends vscode.Disposable {
     /**
      * A request to determine whether this provider can provide a code browsing configuration for the workspace folder.
      * @param token (optional) The cancellation token.
-     * @returns 'true' if this provider can provider a code browsing configuration for the workspace folder.
+     * @returns 'true' if this provider can provide a code browsing configuration for the workspace folder.
      */
     canProvideBrowseConfiguration(token?: CancellationToken): Thenable<boolean>;
 
     /**
      * A request to get the code browsing configuration for the workspace folder.
+     * @param token (optional) The cancellation token.
      * @returns A [WorkspaceBrowseConfiguration](#WorkspaceBrowseConfiguration) with the information required to
      * construct the equivalent of `browse.path` from `c_cpp_properties.json`.
      */
     provideBrowseConfiguration(token?: CancellationToken): Thenable<WorkspaceBrowseConfiguration>;
+
+    /**
+     * A request to determine whether this provider can provide a code browsing configuration for each folder in a multi-root workspace.
+     * @param token (optional) The cancellation token.
+     * @returns 'true' if this provider can provide a code browsing configuration for each folder in a multi-root workspace.
+     */
+    canProvideBrowseConfigurationsPerFolder(token?: CancellationToken): Thenable<boolean>;
+
+    /**
+     * A request to get the code browsing configuration for the workspace folder.
+     * @param uri The URI of the folder to provide a browse configuration for.
+     * @param token (optional) The cancellation token.
+     * @returns A [WorkspaceBrowseConfiguration](#WorkspaceBrowseConfiguration) with the information required to
+     * construct the equivalent of `browse.path` from `c_cpp_properties.json`.
+     */
+    provideFolderBrowseConfiguration(uri: vscode.Uri, token?: CancellationToken): Thenable<WorkspaceBrowseConfiguration>;
 }
 
 /**
@@ -120,6 +137,7 @@ export interface SourceFileConfiguration {
     /**
      * This must also include the system include path (compiler defaults) unless
      * [compilerPath](#SourceFileConfiguration.compilerPath) is specified.
+     * Mac framework paths may also be added to this array.
      */
     readonly includePath: string[];
 
@@ -183,7 +201,7 @@ export interface SourceFileConfigurationItem {
 }
 
 /**
- * The model representing the source browsing configuration for the workspace folder.
+ * The model representing the source browsing configuration for a workspace folder.
  */
 export interface WorkspaceBrowseConfiguration {
     /**
